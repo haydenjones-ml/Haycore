acdef5d844cad94e7c9285e0eb23aae70c5630db
Hayden Jones <hwjones90713@gmail.com>
2026-03-11 23:55:20 -0700

    Update datapath.v
    datapath updated to reflect RISC-V standards

--- src/datapath/datapath.v
+++ src/datapath/datapath.v
@@ -6,8 +6,9 @@ module datapath (
         input  wire        reg_dst,
         input  wire        we_reg,
         input  wire        alu_src,
-        input  wire        dm2reg,
-        input  wire [2:0]  alu_ctrl,
+        input  wire [1:0]  result_src,
+        input  wire [2:0]  imm_type,
+        input  wire [3:0]  alu_ctrl,
         input  wire [4:0]  ra3,
         input  wire [31:0] instr,
         input  wire [31:0] rd_dm,
@@ -17,23 +18,17 @@ module datapath (
         output wire [31:0] rd3
     );
 
-    wire [4:0]  rf_wa;
     wire        pc_src;
     wire [31:0] pc_plus4;
-    wire [31:0] pc_pre;
+    wire [31:0] pc_target;
     wire [31:0] pc_next;
-    wire [31:0] sext_imm;
-    wire [31:0] ba;
-    wire [31:0] bta;
-    wire [31:0] jta;
+    wire [31:0] ext_imm;
     wire [31:0] alu_pa;
     wire [31:0] alu_pb;
     wire [31:0] wd_rf;
     wire        zero;
     
-    assign pc_src = branch & zero;
-    assign ba = {sext_imm[29:0], 2'b00};
-    assign jta = {pc_plus4[31:28], instr[25:0], 2'b00};
+    assign pc_src = jump | branch & zero;
     
     // --- PC Logic --- //
     dreg pc_reg (
@@ -49,41 +44,27 @@ module datapath (
             .y              (pc_plus4)
         );
 
-    adder pc_plus_br (
-            .a              (pc_plus4),
-            .b              (ba),
-            .y              (bta)
+    adder pc_target (
+            .a              (pc_current),
+            .b              (ext_imm),
+            .y              (pc_target)
         );
 
-    mux2 #(32) pc_src_mux (
-            .sel            (pc_src),
-            .a              (pc_plus4),
-            .b              (bta),
-            .y              (pc_pre)
-        );
-
-    mux2 #(32) pc_jmp_mux (
-            .sel            (jump),
-            .a              (pc_pre),
-            .b              (jta),
-            .y              (pc_next)
+    // -- Immediate Logic --- //
+    imm_gen imm_gen (
+            .instr          (instr),
+            .imm_type       (imm_type),
+            .ext_imm        (ext_imm)
         );
 
     // --- RF Logic --- //
-    mux2 #(5) rf_wa_mux (
-            .sel            (reg_dst),
-            .a              (instr[20:16]),
-            .b              (instr[15:11]),
-            .y              (rf_wa)
-        );
-
     regfile rf (
             .clk            (clk),
             .we             (we_reg),
-            .ra1            (instr[25:21]),
-            .ra2            (instr[20:16]),
+            .ra1            (instr[19:15]),
+            .ra2            (instr[24:20]),
             .ra3            (ra3),
-            .wa             (rf_wa),
+            .wa             (instr[11:7]),
             .wd             (wd_rf),
             .rd1            (alu_pa),
             .rd2            (wd_dm),
@@ -107,12 +88,9 @@ module datapath (
             .y              (alu_out)
         );
 
-    // --- MEM Logic --- //
-    mux2 #(32) rf_wd_mux (
-            .sel            (dm2reg),
-            .a              (alu_out),
-            .b              (rd_dm),
-            .y              (wd_rf)
-        );
+    // --- MEM Logic --- // (Changed to accomodate JAL/JALR split)
+    assign wd_rf = (result_src == 2'b01) ? rd_dm :
+                    (result_src == 2'b10) ? pc_plus4 :
+                    alu_out;
 
 endmodule
